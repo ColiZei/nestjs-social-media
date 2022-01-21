96e2ff61257292a9d5c629d3da33e456a527425c
Sebastian Collazo-Quaas <mission@collazo.de>
2022-01-21 21:28:10 +0100

    Add pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
-import { router } from "./router/router";
 import App from "./App.vue";
+import { router } from "./router/router";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
+app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
