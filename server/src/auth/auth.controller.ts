e59612e9be55758b0d39f5c16beb27ca73d82490
Sebastian Collazo-Quaas <mission@collazo.de>
2022-02-01 22:05:21 +0100

    Add register user #4

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register.-user.dto';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return req.user;
+  }
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { RegisterUserDto } from './dto/register.-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -22,5 +27,18 @@ export class AuthService {
 
   verifyPassword(plainPassword: string, hashedPassword: string) {
     return bcrypt.compare(plainPassword, hashedPassword);
+  }
+
+  async register(registerUserDto: RegisterUserDto) {
+    let user = null;
+    try {
+      user = await this.userService.create(registerUserDto);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new BadRequestException('User with that email already exists');
+      }
+      throw new InternalServerErrorException();
+    }
+    return user;
   }
 }
